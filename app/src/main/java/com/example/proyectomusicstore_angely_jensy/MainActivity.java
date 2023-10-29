4ae1a726c2314713435c6aace2cbd6b82ddfb369
jvillafranca <jensylorena2402@gmail.com>
2023-10-28 21:37:10 -0600

    Initial commit

---
+++ app/src/main/java/com/example/proyectomusicstore_angely_jensy/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.proyectomusicstore_angely_jensy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+    Button prueba;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        prueba =(Button)findViewById(R.id.boton);
+
+        prueba.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent prueba = new Intent(getApplicationContext(),activity_registrarse.class);
+                startActivity(prueba);
+            }
+        });
+    }
+}
