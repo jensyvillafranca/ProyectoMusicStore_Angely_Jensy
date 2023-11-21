ecf50e7cd931d51ffcb395426a2b1fa5414ee8d4
jvillafranca <jensylorena2402@gmail.com>
2023-11-21 09:08:03 -0600

    Jensy

---
+++ app/src/main/java/com/example/proyectomusicstore_angely_jensy/Activity_Conf_Tipo_Dato_Biometrico.java
@@ -0,0 +1,14 @@
+package com.example.proyectomusicstore_angely_jensy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Conf_Tipo_Dato_Biometrico extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conf_tipo_dato_biometrico);
+    }
+}
---
+++ app/src/main/java/com/example/proyectomusicstore_angely_jensy/OpenCVPrueba.java
@@ -0,0 +1,14 @@
+package com.example.proyectomusicstore_angely_jensy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OpenCVPrueba extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open_cvprueba);
+    }
+}
